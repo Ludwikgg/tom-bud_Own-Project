0da4b6b6fb90c9478a370690f40acdd107ab1a49
Damian <damian.ludwig83@gmail.com>
2023-07-10 22:14:32 +0200

    fix js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const menubtn = document.querySelector(".nav__btn")
+const btnico = document.querySelector(".fa-solid")
 const menulist = document.querySelector(".nav__list")
 document.querySelector(".description__section")
 const navlinks = document.querySelectorAll(".nav__link")
@@ -6,11 +7,31 @@ const navlinks = document.querySelectorAll(".nav__link")
 
 const menuToggle = () => {
     menulist.classList.toggle("active");
+    btnico.classList.toggle("fa-bars-staggered");
+
+    navlinks.forEach(item => {
+        item.addEventListener('click', () => {
+            menulist.classList.remove('active');
+            btnico.classList.remove("fa-bars-staggered")
+        })
+    })
+
+    handleNavItemsAnimation()
 }
+
+const handleNavItemsAnimation = () => {
+    let delayTime = 0;
+
+    navlinks.forEach(item => {
+        item.classList.toggle("nav__link-animation")
+        item.style.animationDelay = '.' + delayTime + 's';
+        delayTime++;
+    })
+}
+
+
+
+
+
+
 menubtn.addEventListener('click', menuToggle)
-
-const menuoff = () => {
-    menulist.classList.remove("active");
-}
-
-navlinks.addEventListener('click', menuoff)
